32e3dcf631868724f91c8ca52d14a6a093dd20ff
Spencer Smith <dontaskspencer@gmail.com>
2017-09-15 16:33:20 -0600

    Footer changes

--- footer.php
+++ footer.php
@@ -15,15 +15,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bountiful-brain' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'bountiful-brain' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'bountiful-brain' ), 'bountiful-brain', '<a href="http://dotspencer.com">Spencer Smith</a>' );
-			?>
+			<div>&copy; <?php echo date("Y"); ?> - Bountiful Brain Center</div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
